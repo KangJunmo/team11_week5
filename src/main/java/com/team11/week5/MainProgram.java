8b0b09e05894b3d0ab512281284ea76e744d8a67
kangjunmo <kangjunmo@kangjunmoui-MacBook-Pro.local>
2016-04-11 18:54:31 +0900

    asdasd

--- src/main/java/com/team11/week5/MainProgram.java
+++ src/main/java/com/team11/week5/MainProgram.java
@@ -19,7 +19,7 @@ public class MainProgram {
 		
 		if(mp.argsLength == 3) {
 			scan = new Scanner(args[0] + " " + args[1] + " " + args[2]);
-		} else if(mp.argsLength > 3) {
+		} if(mp.argsLength > 3) {
 			scan = new Scanner(args[0] + " " + args[1] + " " + args[2]);
 		}
 		
